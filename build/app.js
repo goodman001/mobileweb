628178ac1566aff695ce1f69c8b394ffbd14f9f4
goodman001 <beautyhc001@gmail.com>
2017-07-14 05:24:04 -0700

    0714

--- build/app.js
+++ build/app.js
@@ -32,7 +32,18 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 				}
 			);
         }
-		
+		loadDataLocal(div,endpointUrl,flag,paths) {
+			//console.log(this.that);
+			var that = this.that;
+            var data = [];
+			data= JSON.parse(window.localStorage.getItem("mycourse"));
+			console.log(data);
+			if(data == null || data.length == 0 ){
+				data = [];
+				//itemnow.push({'course_id':resObj['courseid'],'subject':resObj['subject'],'catalog_number':resObj['catalog_number']});
+			}
+			that.notify(div,data,flag,paths); // Notify View(s)
+        }
 		// Add observer functionality to AppModel objects:
 		
 		// Add an observer to the list
@@ -78,6 +89,10 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 			//this.model.addObserver(this.updateView); // Add this View as an Observer
 			this.model.loadData(this.div,url,flag,path);
 		}
+		startmymy(url,flag,path){
+			//this.model.addObserver(this.updateView); // Add this View as an Observer
+			this.model.loadDataLocal(this.div,url,flag,path);
+		}
         updateView(obs, args,flag,path) {
 			console.log(path);
 			if(args == null)
@@ -96,14 +111,16 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 					var loadimgdiv = $(".loading");
 					var titles = $(".co-div-inline-center");
 					titles.eq(0).text(data.subject +" " + data.catalog_number);
-					var texts = $(".co-text");
+					var divs = '<div class="co-title">Course ID</div><div class="co-text">'+ data.course_id +'</div><div class="co-title">Title</div><div class="co-text">'+ data.title +'</div><div class="co-title">UNITS</div><div class="co-text">'+ data.units +'</div><div class="co-title">DESCRIPTION</div><div class="co-text">'+ data.description +'</div><div class="co-title">INSTRUCTIONS</div><div class="co-text">'+ data.instructions[0] +'</div><div class="co-title">PREREQUISITES</div><div class="co-text">'+ data.prerequisites +'</div><div class="co-title">ANTIREQUISITES</div><div class="co-text">'+ data.antirequisites  +'</div><div class="co-title">COREQUISITES</div><div class="co-text">'+ data.corequisites +'</div><div class="co-title">CROSSLISTINGS</div><div class="co-text">'+ data.crosslistings +'</div><div class="co-title">TERMS OFFERED</div><div class="co-text">'+ data.terms_offered[0] +'</div><div class="co-title">NOTES</div><div class="co-text">'+ data.notes +'</div>';
+					$('#coursedetail').append(divs);
+					/*var texts = $(".co-text");
 					console.log(texts[0]);
 					
 					texts.eq(0).text(data.course_id);
 					texts.eq(1).text(data.title);
 					texts.eq(2).text(data.units);
 					texts.eq(3).text(data.description);
-					texts.eq(4).text(data.instructions[0]);
+					texts.eq(4).text(data.instructions[0]);*/
 					loadimgdiv.hide();
 					//coursediv.show();
 					//var btns = $(".footer-btn");
@@ -134,6 +151,16 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 					//var btns = $(".footer-btn");
 					//texts.eq(0).attr("href",)
 					
+				}
+				else if(path == 'mymy'){
+					console.log(args);
+					var data = args;
+					$(".ui-tbody").html("");
+					for(var i=0;i< data.length;i++){
+						var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].title+' </a></td>');
+						$(".ui-tbody").append($("<tr></tr>").append(trc));
+					}
+					$(".loading").hide();
 				}
 				else
 				{
@@ -167,29 +194,30 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 					var data = args.data;
 					var loadimgdiv = $(".loading");
 					//console.log(loadimgdiv);
-					loadimgdiv.hide();
+					
 					$(".ui-tbody").html("");
 					for(var i=0;i< data.length;i++){
 
 						if(flag == -1){
-							var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject +  '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" hhref="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject +  '" >'+data[i].title+' </a></td>');
+							var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].title+' </a></td>');
 							//var tr = $("<td></td>")
 							//var li = $("<td>abc</td>");
 							$(".ui-tbody").append($("<tr></tr>").append(trc));
 						}else if(endindex == -2)
 						{
 							if(data[i].catalog_number >=startindex){
-								var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject +  '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" hhref="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject +  '" >'+data[i].title+' </a></td>');
+								var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].title+' </a></td>');
 								$(".ui-tbody").append($("<tr></tr>").append(trc));
 							}
 						}else{
 							if(data[i].catalog_number >=startindex && data[i].catalog_number <= endindex){
-								var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject +  '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" hhref="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject +  '" >'+data[i].title+' </a></td>');
+								var trc =  $('<td class="firstCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].subject +" " + data[i].catalog_number +'</a></td>'+ '<td class="lastCol"><a class="courselink" href="course.html?courseid='+ data[i].course_id + '&catalog_number='+data[i].catalog_number + '&subject=' +data[i].subject + '&title=' +data[i].title + '" >'+data[i].title+' </a></td>');
 								$(".ui-tbody").append($("<tr></tr>").append(trc));
 							}
 						}
 
 					}
+					loadimgdiv.hide();
 				}
 				
 				
@@ -209,8 +237,11 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 		console.log(path);
 		var model = new AppModel();
 		var view = new AppView(model, "div#viewContents");
+		$(".footer-left").css("background-color","#CACACA");
+		$(".footer-right").css("background-color","#929292");
 		if(path == 'index'){
-			
+			$('#cocodiv').show();
+			$('#mymydiv').hide();
 			view.startindex("https://api.uwaterloo.ca/v2/courses.json?",-1,path);
 			$(".ui-btn").click(function(){
 				var course = $("select[name='course']").val();
@@ -234,13 +265,32 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 				}
 				//if(course !='course' && term == 'term')
 			});
-			$(".ui-courselist").on("click","a", function() {
-				console.log("click");
+			$(".footer-btn-one").click(function(){
+				$('#cocodiv').show();
+				$('#mymydiv').hide();
+				$(".loading").show();
+				$(".footer-left").css("background-color","#CACACA");
+				$(".footer-right").css("background-color","#929292");
+				view.startindex("https://api.uwaterloo.ca/v2/courses.json?",-1,path);
+				//$('.co-btn-back').attr("href","index.html");
+			});
+			$(".footer-btn-sec").click(function() {
+				$('#cocodiv').hide();
+				$('#mymydiv').show();
+				$(".loading").show();
+				$(".footer-left").css("background-color","#929292");
+				$(".footer-right").css("background-color","#CACACA");
+				//view.startother();
+				view.startmymy("localstore",-1,'mymy');
+				//view.startother(tt,-1,'schedule');
+				//$('.sche-btn-back').attr("href","course.html?"+str);
+				//alert($(".footer-btn").attr("href"));
 			});
 			//window.location.href=
 			//$("#viewContent").click(function(){alert("cc")});	
 		}else if(path == 'course')
 		{
+			
 			var targeturl = location.search; //
 			console.log(targeturl);
     		var resObj = new Object();
@@ -255,21 +305,61 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 					resObj[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);   
 				}
 				console.log(resObj);
-				if(resObj['courseid'] !=undefined && resObj['catalog_number'] !=undefined && resObj['subject'] !=undefined){
+				if(resObj['courseid'] !=undefined && resObj['catalog_number'] !=undefined && resObj['subject'] !=undefined && resObj['title'] !=undefined){
 					/*var btns = $(".footer-btn");
 					//console.log(targeturl);
 					btns.eq(0).attr("href","course.html"+targeturl);
 					btns.eq(1).attr("href","schedule.html"+targeturl);*/
 					view.startindex("https://api.uwaterloo.ca/v2/courses/" + resObj['courseid'] + ".json?",-1,path);
+					/*set add or remove*/
+					console.log("normal !");
+					var itemnow = [];
+					itemnow = JSON.parse(window.localStorage.getItem("mycourse"))
+					if(itemnow == null || itemnow.length == 0 ){
+						$('.co-btn-add').show();
+						$('.co-btn-remove').hide();
+					}else
+					{
+						console.log(itemnow);
+						var eqflag = 0;
+						for(var i=0;i<itemnow.length;i++){
+							if(itemnow[i].course_id == resObj['courseid']){
+								eqflag = 1;
+								break;
+							}
+						}
+						console.log(resObj['course_id']);
+						console.log(eqflag);
+						if(eqflag == 0){
+							$('.co-btn-add').show();
+							$('.co-btn-remove').hide();
+						}else
+						{
+							$('.co-btn-add').hide();
+							$('.co-btn-remove').show();
+							eqflag = 0;
+						}
+					}
 					$(".footer-btn-sec").click(function() {
 						$('#coursediv').hide();
 						$('#schedulediv').show();
 						$(".loading").show();
+						$(".footer-left").css("background-color","#929292");
+						$(".footer-right").css("background-color","#CACACA");
 						var tt = "https://api.uwaterloo.ca/v2/courses/" + resObj['subject'] + "/" + resObj['catalog_number'] + "/schedule.json?";
 						console.log(tt);
 						view.startother(tt,-1,'schedule');
 						$('.sche-btn-back').attr("href","course.html?"+str);
 						//alert($(".footer-btn").attr("href"));
+					});
+					$(".footer-btn-one").click(function(){
+						$('#coursediv').show();
+						$('#schedulediv').hide();
+						$(".loading").show();
+						$(".footer-left").css("background-color","#CACACA");
+						$(".footer-right").css("background-color","#929292");
+						view.startindex("https://api.uwaterloo.ca/v2/courses/" + resObj['courseid'] + ".json?",-1,path);
+						//$('.co-btn-back').attr("href","index.html");
 					});
 					$(".sche-btn").click(function(){
 						var term = $("select[name='scheterm']").val();
@@ -279,13 +369,54 @@ var apiKey = '5884f08401ed5110fe86869d137d628a';
 						console.log(tt);
 						view.startother(tt,-1,'schedule');
 					});
-					$(".footer-btn-one").click(function(){
-						$('#coursediv').show();
-						$('#schedulediv').hide();
-						$(".loading").show();
-						view.startindex("https://api.uwaterloo.ca/v2/courses/" + resObj['courseid'] + ".json?",-1,path);
-						//$('.co-btn-back').attr("href","index.html");
+					$(".co-btn-add").click(function(){
+						console.log("add !");
+						itemnow = [];
+						itemnow = JSON.parse(window.localStorage.getItem("mycourse"));
+						console.log(itemnow);
+						if(itemnow == null || itemnow.length == 0 ){
+							itemnow = [];
+							itemnow.push({'course_id':resObj['courseid'],'subject':resObj['subject'],'catalog_number':resObj['catalog_number'],'title':resObj['title']});
+						}else
+						{
+							var eqflag = 0;
+							for(var i=0;i<itemnow.length;i++){
+								if(itemnow[i].course_id == resObj['courseid']){
+									eqflag = 1;
+									break;
+								}
+							}
+							if(eqflag == 0){
+								itemnow.push({'course_id':resObj['courseid'],'subject':resObj['subject'],'catalog_number':resObj['catalog_number'],'title':resObj['title']});
+							}else
+							{
+								console.log("exit!");
+								eqflag = 0;
+							}
+						}
+						console.log(itemnow);
+						window.localStorage.setItem("mycourse",JSON.stringify(itemnow));
+						$('.co-btn-add').hide();
+						$('.co-btn-remove').show();
 					});
+					$(".co-btn-remove").click(function(){
+						var itemnow = JSON.parse(window.localStorage.getItem("mycourse"));
+						var itemtmp = [];
+						for(var i=0;i<itemnow.length;i++){
+							if(itemnow[i].course_id == resObj['courseid']){
+								continue;
+							}else
+							{
+								//console.log(itemnow[i].course_id);
+								itemtmp.push(itemnow[i]);
+							}
+						}
+						console.log(itemtmp);
+						window.localStorage.setItem("mycourse",JSON.stringify(itemtmp));
+						$('.co-btn-add').show();
+						$('.co-btn-remove').hide();
+					});
+					
 					
 				}else
 				{
